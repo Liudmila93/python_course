bbf8631960ea17758d6f06151ddc1537df0f1ec8
Liudmila <lsorokina@dejavoosystems.ru>
2021-11-07 19:16:35 +0700

    Add some rules for using strings

---
+++ Strings/string.py
@@ -0,0 +1,26 @@
+# Symbol is used for commenting
+""" This is used to write a description and for multilines"""
+
+"""We can use "" or '  """
+print("print1: Hello, world")
+"""To break line use \n or 3х quotes"""
+print("print2: Hello, \nworld")
+print("""print3: Hello, this is
+multiline string""")
+
+
+str1 = "Backslash \"double quotes\" to use it inside"
+print("hello \tme")
+"""
+Newline: \n
+Backslash: \\
+Single quote: \'
+Backspace: \b
+Tab: \t
+"""
+
+# \n перенесет каждое слово на новую строку
+print('туториал', 'по', 'функции', 'print()', sep='\n')
+
+# sep разделит слова символом
+print('туториал', 'по', 'функции', 'print()', sep=',')
